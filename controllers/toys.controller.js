f354deaacc3ae0e689518664b2a54730239c3eed
shahjalal-bu <mdshahjalal.bu@gmail.com>
2023-05-21 04:36:48 +0600

    add product api added

--- controllers/toys.controller.js
+++ controllers/toys.controller.js
@@ -1,6 +1,56 @@
+const { query } = require("express");
 const { toysCollection } = require("../db");
+const { ObjectId } = require("mongodb");
 
 module.exports.findAll = async (req, res) => {
-  const toys = await toysCollection.find().toArray();
+  let query =
+    req.params.categoryName.toLowerCase() === "all"
+      ? {}
+      : { Category: req.params.categoryName };
+  let toys = await toysCollection.find(query).toArray();
   res.send(toys);
+};
+
+//find single product with it
+module.exports.findById = async (req, res) => {
+  let product = await toysCollection.findOne({
+    _id: new ObjectId(req.params.id),
+  });
+  res.send(product);
+};
+
+module.exports.findCategory = async (req, res) => {
+  const toys = await toysCollection.find().project({ Category: 1 }).toArray();
+  const categoryArray = ["All"];
+  toys.forEach((cat) => categoryArray.push(cat.Category));
+  const uniqueArray = Array.from(new Set(categoryArray));
+  res.send(uniqueArray);
+};
+
+//create product
+module.exports.insertOne = async (req, res) => {
+  try {
+    const data = req.body;
+    const result = await toysCollection.insertOne(data);
+    res.send(result);
+  } catch (error) {
+    console.error("Failed to insert document:", error);
+    res.status(500).send("Failed to insert document");
+  }
+};
+
+//find single product with it
+module.exports.findAndUpdate = async (req, res) => {
+  try {
+    const documentId = req.params.id;
+    const updatedDocument = req.body;
+    const updatedRes = await toysCollection.updateOne(
+      { _id: new ObjectId(documentId) },
+      { $set: updatedDocument }
+    );
+    res.send(updatedRes);
+  } catch (error) {
+    console.error("Failed to update document:", error);
+    res.status(500).send("Failed to update document");
+  }
 };
--- routes/toys.js
+++ routes/toys.js
@@ -2,6 +2,10 @@ const express = require("express");
 const router = express.Router();
 const toysController = require("../controllers/toys.controller");
 
-router.get("/all", toysController.findAll);
+router.get("/cat/:categoryName", toysController.findAll);
+router.get("/categorynames", toysController.findCategory);
+router.get("/:id", toysController.findById);
+router.post("/", toysController.insertOne);
+router.put("/:id", toysController.findAndUpdate);
 
 module.exports = router;
