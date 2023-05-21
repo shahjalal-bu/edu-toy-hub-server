df8b9148c906e9129d7541257a51710ad409055a
shahjalal-bu <mdshahjalal.bu@gmail.com>
2023-05-21 20:32:07 +0600

    add a html file

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 //configure dot env
 require("dotenv").config();
 const express = require("express");
+const path = require("path");
 const cors = require("cors");
 const { toysCollection } = require("./db");
 const app = express();
@@ -14,8 +15,18 @@ const toysRouter = require("./routes/toys");
 //middleware
 app.use(express.json());
 app.use(cors());
-app.get("/", async (req, res) => {
-  res.send("Server running");
+// app.get("/", async (req, res) => {
+//   res.send("Server running");
+// });
+app.get("/", (req, res) => {
+  const htmlFilePath = path.join(__dirname, "index.html");
+
+  res.sendFile(htmlFilePath, (err) => {
+    if (err) {
+      console.error("Error sending HTML file:", err);
+      res.status(500).send("Internal Server Error");
+    }
+  });
 });
 app.use("/toys", toysRouter);
 
